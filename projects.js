c24d2fd61535b437c572e649014c5173600417af
Yamen <125800880+yamen-alnamly@users.noreply.github.com>
2025-10-17 19:35:11 +0300

    Add files via upload

--- projects.js
+++ projects.js
@@ -1,42 +1,21 @@
 export const projects = [
   {
-    img: "photos/project1c.jpg",
+    img: "projectImages/1.jpg",
     name: "مشروع مشفى عين ترما",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, aliquam explicabo tempora optio omnis enim amet sit iusto, ducimus assumenda non natus voluptate adipisci voluptatem? Non, quae pariatur temporibus doloremque magnam sit animi nulla, magni est voluptas, voluptate repellat! Maiores!",
     id: 1,
     Link: " "
   },
   {
-    img: "photos/project2c.jpg",
+    img: "projectImages/2.jpg",
     name: "مشروع ابينة الحسام في عين ترما",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, aliquam explicabo tempora optio omnis enim amet sit iusto, ducimus assumenda non natus voluptate adipisci voluptatem? Non, quae pariatur temporibus doloremque magnam sit animi nulla, magni est voluptas, voluptate repellat! Maiores!",
     id: 1,
     Link: " "
   },
   {
-    img: "photos/project3c.jpg",
+    img: "projectImages/3.jpg",
     name: "مشروع ابينة الحسام 2 في دوما",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, aliquam explicabo tempora optio omnis enim amet sit iusto, ducimus assumenda non natus voluptate adipisci voluptatem? Non, quae pariatur temporibus doloremque magnam sit animi nulla, magni est voluptas, voluptate repellat! Maiores!",
-    id: 1,
-    Link: " "
-  },
-  {
-    img: "photos/Land.jpg",
-    name: "placeholder",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, aliquam explicabo tempora optio omnis enim amet sit iusto, ducimus assumenda non natus voluptate adipisci voluptatem? Non, quae pariatur temporibus doloremque magnam sit animi nulla, magni est voluptas, voluptate repellat! Maiores!",
-    id: 1,
-    Link: " "
-  },
-  {
-    img: "photos/Land.jpg",
-    name: "placeholder",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, aliquam explicabo tempora optio omnis enim amet sit iusto, ducimus assumenda non natus voluptate adipisci voluptatem? Non, quae pariatur temporibus doloremque magnam sit animi nulla, magni est voluptas, voluptate repellat! Maiores!",
-    id: 1,
-    Link: " "
-  },
-  {
-    img: "photos/Land.jpg",
-    name: "placeholder",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, aliquam explicabo tempora optio omnis enim amet sit iusto, ducimus assumenda non natus voluptate adipisci voluptatem? Non, quae pariatur temporibus doloremque magnam sit animi nulla, magni est voluptas, voluptate repellat! Maiores!",
     id: 1,
     Link: " "
